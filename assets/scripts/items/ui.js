391f1c354a83235172eb922deb11268e01be9796
nathanmailhiot <nathanjm12@outlook.com>
2019-04-29 15:16:21 -0400

    Fix small bugs

--- assets/scripts/items/ui.js
+++ assets/scripts/items/ui.js
@@ -2,48 +2,65 @@ const store = require('../store.js')
 const showItemsTemplate = require('./../templates/helpers/item-listing.handlebars')
 
 const onCreateItemSuccess = function (data) {
-  console.log('Item was created')
   $('form').trigger('reset')
   store.item = data.item
-  console.log(data)
+  $('.messaging').text('New item created check your list!')
+  setTimeout(function () {
+    $('.messaging').text('')
+  }, 2000)
   $('.content').hide()
 }
 
 const onCreateItemFailure = function (data) {
-  console.log('Item was not created')
+  $('.messaging').text('Something went wrong item was not created.')
+  setTimeout(function () {
+    $('.messaging').text('')
+  }, 2000)
 }
 
 const onGetItemsSuccess = function (data) {
-  console.log('Item was found')
-  console.log(data)
   const showItemsHtml = showItemsTemplate({ items: data.items })
   $('.content').html(showItemsHtml)
 }
 
 const onGetItemsFailure = function (data) {
-  console.log('Item was not found')
+  $('.messaging').text('Something went wrong list items were not found.')
+  setTimeout(function () {
+    $('.messaging').text('')
+  }, 2000)
 }
 
 const onUpdateItemSuccess = function (data) {
-  console.log('Item was updated')
   $('form').trigger('reset')
   store.item = data.item
-  console.log(data)
+  $('.messaging').text('Item was updated check the bottom of your list!')
+  setTimeout(function () {
+    $('.messaging').text('')
+  }, 2000)
   const showItemsHtml = showItemsTemplate({ items: data.items })
   $('.content').html(showItemsHtml)
 }
 
 const onUpdateItemFailure = function (data) {
-  console.log('Item was not updated')
+  $('.messaging').text('Something went wrong item was not updated!')
+  setTimeout(function () {
+    $('.messaging').text('')
+  }, 2000)
 }
 
 const onDestroyItemSuccess = function () {
-  console.log('item was deleted')
+  $('.messaging').text('Item was deleted check your list!')
+  setTimeout(function () {
+    $('.messaging').text('')
+  }, 2000)
   $('.content').empty()
 }
 
 const onDestroyItemFailure = function () {
-  console.log('item was not destroyed')
+  $('.messaging').text('Something went wrong item was not deleted.')
+  setTimeout(function () {
+    $('.messaging').text('')
+  }, 2000)
 }
 
 module.exports = {
